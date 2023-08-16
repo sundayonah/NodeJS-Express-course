2d34e81ebb93304444afeeb7459a67579dd292b7
sundayonah <sundayonah94@gmail.com>
2023-08-16 15:51:36 +0100

    create navbar in html and connect with express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,26 @@
-console.log('Express Tutorial')
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("Resources Not Found");
+});
+
+app.listen(3001, () => {
+  console.log("Server listening on port 3001");
+});
+
+//app.get =>
+//app.post
+//app.put
+//app.delete
+//app.all
+//app.use => responsible for middlewares
+//app.listen
