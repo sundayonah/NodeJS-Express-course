7cff06c791564dc36acf9ca90a402cc90254b6b9
sundayonah <sundayonah94@gmail.com>
2023-08-17 09:06:53 +0100

    update app

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,56 +1,26 @@
 const express = require("express");
-const { products } = require("./data");
+const logger = require("./logger");
+
 const app = express();
 
+//req => middleware => res
+app.use(logger);
+
 app.get("/", (req, res) => {
-  res.send('<h1>Home Page!</h1><a href="/api/products">Products</a>');
+  res.send("Home");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About");
 });
 
 app.get("/api/products", (req, res) => {
-  const newProduct = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.json(newProduct);
+  res.send("Products");
 });
 
-app.get("/api/products/:productID", (req, res) => {
-  // console.log(req);
-  // console.log(req.params);
-  const { productID } = req.params;
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  );
-  if (!singleProduct) {
-    return res.status(404).send("Product not found");
-  }
-  res.json(singleProduct);
+app.get("/api/items", (req, res) => {
+  res.send("items");
 });
-
-app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
-  console.log(req.params);
-  res.send("Hello World!");
-});
-
-app.get("/api/v1querry", (req, res) => {
-  console.log(req.query);
-  // const { search, limit } = req.query;
-  // let sortedProducts = [...products];
-
-  // if (search) {
-  //   sortedProducts = sortedProducts.filter((product) => {
-  //     return product.name.startsWith(search);
-  //   });
-  // }
-
-  // if (limit) {
-  //   sortedProducts = sortedProducts.slice(0, Number(limit));
-  // }
-  // res.status(200).json(sortedProducts);
-  res.send("Hello World!");
-});
-
-app.get();
 
 app.listen(3001, () => {
   console.log("Server listening on port 3001");
