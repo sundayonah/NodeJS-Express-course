6260380498df594b08ee1dcbd57ade73776fe8e7
sundayonah <sundayonah94@gmail.com>
2023-08-18 13:10:02 +0100

    mongooseDB setup

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,37 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+// app.get('/api/v1/tasks')         => get all the tasks
+// app.post('/api/v1/tasks')        => create a new task
+// app.get('/api/v1/tasks/:id')     => get single task
+// app.patch('/api/v1/tasks/:id')   => update task
+// app.delete('/api/v1/tasks/:id')  => delete single task
+
+const port = 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on ${port}...`));
+  } catch (error) {
+    console.log(error, "Error connecting DB");
+  }
+};
+
+start();
+
+// mongodb pwd =>  mZy1eYF3aLTzy7RV
+//mongodb+srv://xhunteq:<password>@cluster0.vmgxy32.mongodb.net/?retryWrites=true&w=majority
