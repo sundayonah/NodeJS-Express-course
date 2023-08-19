99990e7cbe74df2d340b5ff77cb4ce4a676b6809
sundayonah <sundayonah94@gmail.com>
2023-08-19 14:45:24 +0100

    create db with jsonProducts Array

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,43 @@
-console.log('04 Store API')
+require("dotenv").config();
+
+// async error
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+
+const productsRouter = require("./routes/products");
+
+const notFoundMiddelware = require("./middleware/not-found");
+const errorMiddelware = require("./middleware/error-handler");
+
+// middleware
+app.use(express.json());
+
+//routes
+
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">Products Route</a>');
+});
+app.use("/api/v1/products", productsRouter);
+
+// products route
+
+app.use(notFoundMiddelware);
+app.use(errorMiddelware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    // connectDB
+    app.listen(port, console.log(`Server connect ON port: ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ 04-store-api/starter/controllers/products.js
@@ -0,0 +1,13 @@
+const getAllProductsStatic = async (req, res) => {
+  throw new Error("testing error");
+  res.status(200).json({ msg: "products testing route" });
+};
+
+const getAllProducts = async (req, res) => {
+  res.status(200).json({ msg: "products route" });
+};
+
+module.exports = {
+  getAllProductsStatic,
+  getAllProducts,
+};
