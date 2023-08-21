73d83cdff97c6582cad24ab099fa137cd5d0afba
sundayonah <sundayonah94@gmail.com>
2023-08-21 07:08:02 +0100

    create files and functionality

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -1,19 +1,25 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
 const app = express();
 
+//connectBD
+const connectBD = require("./db/connect");
+
+//routers
+const jobRouter = require("./routes/jobs");
+const authRouter = require("./routes/auth");
+
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use("api/v1/auth", authRouter);
+app.use("api/v1/jobs", jobRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -22,6 +28,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectBD(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
